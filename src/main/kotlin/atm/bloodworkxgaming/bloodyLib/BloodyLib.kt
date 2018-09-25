31a77f56bad1d2f21e1e8f8f16f210a9069456ef
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2018-09-26 00:55:55 +0200

    first release version 1.0.0

--- src/main/kotlin/atm/bloodworkxgaming/bloodyLib/BloodyLib.kt
+++ src/main/kotlin/atm/bloodworkxgaming/bloodyLib/BloodyLib.kt
@@ -8,11 +8,12 @@ import net.minecraftforge.fml.common.Mod
 @Mod(
         modid = MOD_ID,
         modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter",
+        dependencies = "required:forgelin",
         version = VERSION,
         name = NAME
 )
 object BloodyLib {
     const val MOD_ID = "bloodylib"
     const val NAME = "bloodylib"
-    const val VERSION = "0.1"
+    const val VERSION = "@VERSION@"
 }
